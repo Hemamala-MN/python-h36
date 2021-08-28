2b3b78e0a37cb8f8697d75186a29830a56b1d434
Hemamala-MN <84277583+Hemamala-MN@users.noreply.github.com>
2021-08-28 13:13:16 +0530

    Update Activity_03.py

---
+++ Activity_03.py
@@ -0,0 +1,12 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> s1=input("Enter the first string: ")
+Enter the first string: Good
+>>> s2=input("Enter the second string: ")
+Enter the second string: Morning
+>>> s3=s1+s2
+>>> print("Concatenated string: ",s3)
+Concatenated string:  GoodMorning
+>>> print(s3*5)
+GoodMorningGoodMorningGoodMorningGoodMorningGoodMorning
+>>> 
